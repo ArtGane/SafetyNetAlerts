db2454ae4c5b967167b3797e877963886ca9056b
Argane Samson <argane.samson@gmail.com>
2022-05-13 18:10:00 +0200

    structure for endpoints

--- src/main/java/com/safetynet/SafetyNetAlerts/model/FirestationModel.java
+++ src/main/java/com/safetynet/SafetyNetAlerts/model/FirestationModel.java
@@ -1,5 +1,23 @@
 package com.safetynet.SafetyNetAlerts.model;
 
+import lombok.Data;
+
+@Data
 public class FirestationModel {
 
+    private String address;
+    private int station;
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String adress) {
+        this.address = adress;
+    }
+    public int getStation() {
+        return station;
+    }
+    public void setStation(int station) {
+        this.station = station;
+    }
 }
